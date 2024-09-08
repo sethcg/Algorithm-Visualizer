d18290e085936e8d1905d5ee3527d4ed5d0de3db
sethcg <sethcg@hotmail.com>
2024-09-08 13:36:42 -0400

    fixed pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://sethcg.github.io/algorithm-visualizer/',
+  base: './',
   plugins: [react()],
 })
